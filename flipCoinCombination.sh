daeb8e4e88d71fe1712193be62c2de8b6ac0fd5b
Sivakumar-U <usivakumarreddy@gmail.com>
2020-08-05 18:08:57 +0530

    [Siva]Added:To flip a coin for multiple times and store the singlet combination in dictionary to find percentage of the combination

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,11 +1,22 @@
 #!/bin/bash -x
 
-isHeads=1
-randomCheck=$((RANDOM%2))
+declare -A dictionary
 
-if [ $randomCheck -eq $isHeads ]
-then
-	echo "Heads"
-else
-	echo "Tails"
-fi
+dictionary[Heads]=0
+dictionary[Tails]=0
+isHeads=1
+
+for (( i=0; i<10; i++ ))
+do
+	randomCheck=$((RANDOM%2))
+
+	if [ $randomCheck -eq $isHeads ]
+	then
+		((dictionary[Heads]++))
+	else
+		((dictionary[Tails]++))
+	fi
+done
+
+echo "Heads percentage: $((${dictionary[Heads]}*100/10)) percent"
+echo "Tails percentage: $((${dictionary[Tails]}*100/10)) percent"
